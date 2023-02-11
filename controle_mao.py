4434652cb45455bf1cfe9e2b2066faa319503a3b
Hernani Samuel Diniz <86131133+HernaniSamuel@users.noreply.github.com>
2023-02-11 18:05:37 -0300

    Limpeza e adaptação
    Nomes de variáveis alterados para melhor compreensão e adaptação do código para diferentes tamanhos de tela

--- controle_mao.py
+++ controle_mao.py
@@ -5,8 +5,6 @@ from time import sleep
 import speech_recognition as sr
 import pygame
 
-
-
 pygame.init()
 
 video = cv2.VideoCapture(0)
@@ -14,6 +12,11 @@ som = pygame.mixer.Sound('smw_stomp.wav')
 hand = mp.solutions.hands
 Hand = hand.Hands(max_num_hands=1)
 mpDraw = mp.solutions.drawing_utils
+
+
+largura, altura = pyautogui.size()
+metade_largura = int(largura/2)
+metade_altura = int(altura/2)
 
 
 def ouvir_microfone():
@@ -28,59 +31,70 @@ def ouvir_microfone():
     except sr.UnknownValueError:
         print('Não entendi')
         frase = ''
-    return(frase)
+    return (frase)
+
 
 while True:
     check, img = video.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = Hand.process(imgRGB)
     handsPoints = results.multi_hand_landmarks
-    w, h,_ = img.shape
+    w, h, _ = img.shape
     pontos = []
     pyautogui.FAILSAFE = False
     if handsPoints:
         for points in handsPoints:
-
             mpDraw.draw_landmarks(img, points, hand.HAND_CONNECTIONS)
             for id, cord in enumerate(points.landmark):
                 cx, cy = int((cord.x*1920-1920)*-1), int(cord.y*1080)
                 pontos.append((cx, cy))
 
-            #Quadros da tela
-            if pontos[9][0] < 960 and pontos[9][1] < 540:
-                xhis = 960-pontos[9][0]
-                ypsolon = 540 - pontos[9][1]
-                pyautogui.moveTo(pontos[9][0] - xhis, pontos[9][1] - ypsolon)
+            # Quadros da tela (Dividi a tela em quatro quadros para amplificar o movimento do cursor)
+            # superior esquerdo
+            if pontos[9][0] < metade_largura and pontos[9][1] < metade_altura:
+                horizontal = metade_largura - pontos[9][0]
+                vertical = metade_altura - pontos[9][1]
+                pyautogui.moveTo(pontos[9][0] - horizontal, pontos[9][1] - vertical)
 
-            if pontos[9][0] > 960 and pontos[9][1] < 540:
-                ypsolon = 540 - pontos[9][1]
-                xhis = pontos[9][0] - 960
-                pyautogui.moveTo(pontos[9][0] + xhis, pontos[9][1] - ypsolon)
+            # superior direito
+            if pontos[9][0] > metade_largura and pontos[9][1] < metade_altura:
+                vertical = metade_altura - pontos[9][1]
+                horizontal = pontos[9][0] - metade_largura
+                pyautogui.moveTo(pontos[9][0] + horizontal, pontos[9][1] - vertical)
 
-            if pontos[9][0] < 960 and pontos[9][1] > 540:
-                xhis = 960-pontos[9][0]
-                ypsolon = pontos[9][1] - 540
-                pyautogui.moveTo(pontos[9][0] - xhis, pontos[9][1] + ypsolon)
+            # inferior esquerdo
+            if pontos[9][0] < metade_largura and pontos[9][1] > metade_altura:
+                horizontal = metade_largura - pontos[9][0]
+                vertical = pontos[9][1] - metade_altura
+                pyautogui.moveTo(pontos[9][0] - horizontal, pontos[9][1] + vertical)
 
-            if pontos[9][0] > 960 and pontos[9][1] > 540:
-                ypsolon = pontos[9][1] - 540
-                xhis = pontos[9][0] - 960
-                pyautogui.moveTo(pontos[9][0] + xhis, pontos[9][1] + ypsolon)
+            # inferior direito
+            if pontos[9][0] > metade_largura and pontos[9][1] > metade_altura:
+                vertical = pontos[9][1] - metade_altura
+                horizontal = pontos[9][0] - metade_largura
+                pyautogui.moveTo(pontos[9][0] + horizontal, pontos[9][1] + vertical)
 
-            #comandos
+            # comandos
+
+            # Clique
             if pontos[8][1] >= pontos[7][1] and pontos[20][1] <= pontos[19][1]:
                 pyautogui.click()
                 sleep(0.2)
 
+            # Segurar e arrastar
             if pontos[20][1] >= pontos[19][1]:
                 pyautogui.mouseDown()
+
+            # Soltar
             if pontos[20][1] < pontos[19][1]:
                 pyautogui.mouseUp()
 
+            # Clique com o botão direito
             if pontos[12][1] >= pontos[11][1] and pontos[20][1] <= pontos[19][1]:
                 pyautogui.rightClick()
                 sleep(0.2)
 
+            # Pesquisa por voz
             if pontos[16][1] >= pontos[15][1] and pontos[20][1] <= pontos[19][1]:
                 sleep(0.3)
                 pyautogui.write(ouvir_microfone())
